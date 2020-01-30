59e8705daff1e7c9903d8176250b7765bfca0c3e
Ben Kero <bkero@bke.ro>
2020-01-30 13:29:41 +0100

    remove commented out line in auth

--- auth.js
+++ auth.js
@@ -3,7 +3,6 @@ const config = require('./config.js');
 
 module.exports = function(request, response, next) {
   var user = auth(request)
-  //if (!user || process.env.USERNAME !== user.name || process.env.PASSWORD !== user.pass) {
   if (!user || config.username !== user.name || config.password !== user.pass) {
     response.set('WWW-Authenticate', 'Basic realm="example"')
     return response.status(401).send()
